23b541abdc361cc1c782dc406def92d7e5198456
maikofelix47 <maikofelix89@gmail.com>
2023-05-13 17:01:07 +0300

    FE: Fixed AppcComponent tests

--- apps/front-end/src/app/app.component.spec.ts
+++ apps/front-end/src/app/app.component.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from '../app/error/error.component';
+import { LoaderComponent } from '../app/loader/loader.component';
+import { LoaderService } from './services/loader.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,8 +12,13 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ErrorComponent,
+        LoaderComponent
       ],
+      providers:[
+        LoaderService
+      ]
     }).compileComponents();
   });
 
@@ -24,12 +32,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng-ecommerce');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-ecommerce app is running!');
   });
 });
