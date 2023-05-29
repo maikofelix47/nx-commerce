4aaf1ad042087999b3e7a5e3c2c3a98c57f9d11a
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 19:03:51 +0300

    BE: Fixed product controller test

--- apps/back-end/src/app/product/product.controller.spec.ts
+++ apps/back-end/src/app/product/product.controller.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
+
+const mockProductService = {
+
+};
 
 describe('ProductController', () => {
   let controller: ProductController;
+  let productsService: ProductService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: ProductService,
+          useValue: mockProductService
+        }
+      ],
       controllers: [ProductController],
     }).compile();
 
     controller = module.get<ProductController>(ProductController);
+    productsService = module.get<ProductService>(ProductService)
   });
 
-  it('should be defined', () => {
+  it('product controller should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('product service should be defined', () => {
+    expect(productsService).toBeDefined();
+  });
+
 });
