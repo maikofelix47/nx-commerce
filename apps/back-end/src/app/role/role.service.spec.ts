e5c7200b9aa51118e7944ab812672021b1b62421
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 14:44:25 +0300

    BE: Fixed role service test

--- apps/back-end/src/app/role/role.service.spec.ts
+++ apps/back-end/src/app/role/role.service.spec.ts
@@ -1,15 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { RoleService } from './role.service';
+import { Role } from './role.entity';
+import { Repository } from 'typeorm';
+
+
+const FakeRoleRepository = {
+  find: jest.fn(),
+  findBy: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn()
+};
+
+const ROLE_REPOSITORY_TOKEN = getRepositoryToken(Role);
 
 describe('RoleService', () => {
   let service: RoleService;
+  let roleRepository: Repository<Role>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoleService],
+      providers: [
+        RoleService,
+        {
+          provide: ROLE_REPOSITORY_TOKEN,
+          useValue: FakeRoleRepository
+        }
+      ],
     }).compile();
 
     service = module.get<RoleService>(RoleService);
+    roleRepository = module.get<Repository<Role>>(ROLE_REPOSITORY_TOKEN);
   });
 
   it('should be defined', () => {
