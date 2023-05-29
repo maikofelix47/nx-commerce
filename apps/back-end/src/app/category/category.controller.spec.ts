06e523d6b3934b7cc1351fa984328dcce693c7d2
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 19:29:02 +0300

    BE: Fixed category controller test

--- apps/back-end/src/app/category/category.controller.spec.ts
+++ apps/back-end/src/app/category/category.controller.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryController } from './category.controller';
+import { CategoryService } from './category.service';
+
+const mockCategoryService = {
+
+};
 
 describe('CategoryController', () => {
   let controller: CategoryController;
+  let categoryService: CategoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: CategoryService,
+          useValue: mockCategoryService
+        }
+      ],
       controllers: [CategoryController],
     }).compile();
 
     controller = module.get<CategoryController>(CategoryController);
+    categoryService = module.get<CategoryService>(CategoryService);
   });
 
-  it('should be defined', () => {
+  it('category controller should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('category service should be defined', () => {
+    expect(categoryService).toBeDefined();
   });
 });
