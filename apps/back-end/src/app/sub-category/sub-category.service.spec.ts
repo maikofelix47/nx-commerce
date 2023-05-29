3fd5ef05bf5338509f975fba314c1bd92234c8b8
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 19:51:52 +0300

    BE: Fixed subcategory service test

--- apps/back-end/src/app/sub-category/sub-category.service.spec.ts
+++ apps/back-end/src/app/sub-category/sub-category.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SubCategoryService } from './sub-category.service';
+import { Repository } from 'typeorm';
+import { SubCategory } from './sub-category.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockSubCategoryRepo = {
+
+};
+
+const SUB_CATEGORY_TOKEN = getRepositoryToken(SubCategory);
+
 
 describe('SubCategoryService', () => {
   let service: SubCategoryService;
+  let subCategoryRepo: Repository<SubCategory>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SubCategoryService],
+      providers: [
+        SubCategoryService,
+        {
+          provide: SUB_CATEGORY_TOKEN,
+          useValue: mockSubCategoryRepo
+        }
+      ],
     }).compile();
 
     service = module.get<SubCategoryService>(SubCategoryService);
+    subCategoryRepo = module.get<Repository<SubCategory>>(SUB_CATEGORY_TOKEN);
   });
 
-  it('should be defined', () => {
+  it('subcategory service should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('subcategory repo should be defined', () => {
+    expect(subCategoryRepo).toBeDefined();
   });
 });
