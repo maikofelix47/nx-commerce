ea3efb0f047227d915177a33cd0e622c41e757be
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 14:32:18 +0300

    Fixed broken authcontroller tests

--- apps/back-end/src/app/auth/auth.controller.spec.ts
+++ apps/back-end/src/app/auth/auth.controller.spec.ts
@@ -1,15 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { User } from '../users/user.entity';
+import { AuthService } from './auth.service';
+
+class FakeAuthService {
+  public signUp(
+    userName: string,
+    email: string,
+    password: string
+  ): Promise<Partial<User>> {
+    return Promise.resolve({ id: 1, email: email });
+  }
+}
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthService,
+          useClass: FakeAuthService
+        }
+      ],
       controllers: [AuthController],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService)
   });
 
   it('should be defined', () => {
