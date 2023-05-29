8bc028c1fb34b7e58c9ab102c7c703c5a81aefed
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 18:54:47 +0300

    BE: Fixed category service test

--- apps/back-end/src/app/category/category.service.spec.ts
+++ apps/back-end/src/app/category/category.service.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { Repository } from 'typeorm';
+import { Category } from './category.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const fakeCategoryRepo = {
+
+};
+
+const CATEGORY_REPO_TOKEN = getRepositoryToken(Category);
 
 describe('CategoryService', () => {
   let service: CategoryService;
+  let categoryRepo: Repository<Category>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [
+        CategoryService,
+        {
+          provide: CATEGORY_REPO_TOKEN,
+          useValue: fakeCategoryRepo
+        }
+      ],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
+    categoryRepo = module.get<Repository<Category>>(CATEGORY_REPO_TOKEN);
   });
 
-  it('should be defined', () => {
+  it('category service should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('category repository should be defined', () => {
+    expect(categoryRepo).toBeDefined();
   });
 });
