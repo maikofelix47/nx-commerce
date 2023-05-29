887b6c8215aeecb9b53e393c32260c896cc3cdf8
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 18:49:37 +0300

    BE: Fixed permission controller test

--- apps/back-end/src/app/permission/permission.controller.spec.ts
+++ apps/back-end/src/app/permission/permission.controller.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PermissionService } from './permission.service';
 import { PermissionController } from './permission.controller';
+
+class FakePermissionService{
+
+};
 
 describe('PermissionController', () => {
   let controller: PermissionController;
+  let permissionService: PermissionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: PermissionService,
+          useClass: FakePermissionService
+        }
+      ],
       controllers: [PermissionController],
     }).compile();
 
     controller = module.get<PermissionController>(PermissionController);
+    permissionService = module.get<PermissionService>(PermissionService)
   });
 
-  it('should be defined', () => {
+  it('controller should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('permission service should be defined', () => {
+    expect(permissionService).toBeDefined();
   });
 });
