6f18c78d7df6cd03224abbd52a63b6a8f27e237f
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 14:57:30 +0300

    BE: Fixed users controller test

--- apps/back-end/src/app/users/users.controller.spec.ts
+++ apps/back-end/src/app/users/users.controller.spec.ts
@@ -1,15 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+const fakeUsersService = {
+  findAll: jest.fn(()=>{
+    return Promise.resolve([{
+      id: 1,
+      email: "test22@gmail.com"
+     }]);
+  }),
+  findOne: jest.fn((id: number)=> {
+    return Promise.resolve({
+      id: id,
+      email: "test22@gmail.com"
+     });
+
+  }),
+  updateUserPassword: jest.fn((id: number, password: string)=>{
+       return Promise.resolve({
+        id: id,
+        email: "test22@gmail.com"
+       });
+  }),
+  remove: jest.fn(()=>{
+     return Promise.resolve();
+  })
+};
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+       {
+        provide: UsersService,
+        useValue: fakeUsersService
+       }
+      ],
       controllers: [UsersController],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService)
   });
 
   it('should be defined', () => {
