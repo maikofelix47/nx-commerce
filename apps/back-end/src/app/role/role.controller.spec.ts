414a8ed887b24370022c5070d1744d4b518849ef
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 19:35:40 +0300

    BE: Fixed role controller test

--- apps/back-end/src/app/role/role.controller.spec.ts
+++ apps/back-end/src/app/role/role.controller.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoleController } from './role.controller';
+import { RoleService } from './role.service';
+
+const mockRoleService = {
+
+};
 
 describe('RoleController', () => {
   let controller: RoleController;
+  let roleService: RoleService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers:[
+        {
+          provide: RoleService,
+          useValue: mockRoleService
+        }
+      ],
       controllers: [RoleController],
     }).compile();
 
     controller = module.get<RoleController>(RoleController);
+    roleService = module.get<RoleService>(RoleService);
   });
 
-  it('should be defined', () => {
+  it('role controller should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('role service should be defined', () => {
+    expect(roleService).toBeDefined();
   });
 });
