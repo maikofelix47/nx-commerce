a100dee1ae30af6d789c9ef417f9ed428df711b7
maikofelix47 <maikofelix89@gmail.com>
2023-05-29 19:57:09 +0300

    BE: Fixed auth controller test

--- apps/back-end/src/app/auth/auth.service.spec.ts
+++ apps/back-end/src/app/auth/auth.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+const mockUsersService = {
+
+};
+
+const mockJwtService = {
+
+};
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersService: UsersService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: mockUsersService
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService
+        }
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
-  it('should be defined', () => {
+  it('auth service should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('users service should be defined', () => {
+    expect(usersService).toBeDefined();
+  });
+  it('jwt service should be defined', () => {
+    expect(jwtService).toBeDefined();
   });
 });
